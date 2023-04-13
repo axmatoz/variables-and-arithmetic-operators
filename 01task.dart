03560dbec115a16c02d59e3684d3ef9d7450aac1
axmatoz <axmatozodbek@gmail.com>
2023-04-13 10:47:17 +0500

    01task.dart

--- 01task.dart
+++ 01task.dart
@@ -4,4 +4,9 @@
 
 // Find the sum of  'num_one' and 'num_two' then assign to 'variable_total'
 
-void main() {}
+void main() {
+  int x = 5;
+  int y = 4;
+int varible_total= x+=y;
+  print(varible_total);
+}
